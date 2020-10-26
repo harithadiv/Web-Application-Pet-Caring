c0205cf6410eff8aab2d3255d30bf1a0ff4cebd6
nishanthelango <nishanthelango@yahoo.com.sg>
2020-10-26 19:50:39 +0800

    add basic routes

--- App/routes/admin.js
+++ App/routes/admin.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.render("page", { title: "Page" });
-});
+router.get("/", function (req, res, next) {});
 
 module.exports = router;
