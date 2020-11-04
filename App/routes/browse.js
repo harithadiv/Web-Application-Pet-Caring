82f591380a5e31a9fdfc47d088c414ea9a9489b3
harithadiv <haritha.divakar@gmail.com>
2020-11-04 15:27:14 +0800

    add browse page frontend

--- App/routes/browse.js
+++ App/routes/browse.js
@@ -3,14 +3,16 @@ var router = express.Router();
 const sql_query = require("../sql");
 
 const { Pool } = require("pg");
+const { middleware } = require("../auth");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-router.get("/", function (req, res, next) {
+router.get("/", middleware(), function (req, res, next) {
   pool.query(sql_query.query.browse, [], (err, data) => {
     console.log(data.rows);
-    res.render("browse", { avails: data.rows });
+    var username = req.session.passport.user;
+    res.render("browse", { avails: data.rows, username: username });
   });
 });
 
